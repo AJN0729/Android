19b46d8fdbab1ace661e379ab2b2f35184bf4c04
AJN0729 <andrewn0729@student.k12.tulsaschools.org>
2022-01-04 10:47:30 -0600

    day one finished.

--- app/src/main/java/com/example/newprojecttwo/MainActivity.kt
+++ app/src/main/java/com/example/newprojecttwo/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.newprojecttwo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    internal var score = 0
+
+    internal lateinit var tapMeButton: Button
+    internal lateinit var gameScoreTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tapMeButton = findViewById(R.id.tapMeButton)
+
+        tapMeButton.setOnClickListener { view ->
+            incrementScore()
+        }
+    }
+
+    private fun incrementScore() {
+        TODO("Not yet implemented")
     }
 }
